d277e4f270184b16ad7fe8d8022033e6f1733595
Pham Le Minh <leminh032003@gmail.com>
2023-09-13 21:09:29 +0700

    New Commit

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,15 +1,22 @@
 package com.example.myapplication;
 
+import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
-import androidx.appcompat.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
+    String[] array = {"ABC","DFS","kfsof"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayAdapter adapter = new ArrayAdapter(this, R.layout.item_view, R.id.itemViewText ,array);
+
+        ListView listView = findViewById(R.id.list);
+        listView.setAdapter(adapter);
     }
 }
